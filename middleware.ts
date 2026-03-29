54c65b150ef3de818998021e09c2446d58c17ee0
Ricardo Cidale <ricardo.cidale@norfolk.ai>
2026-03-29 03:25:09 -0500

    fix: update middleware for Clerk v7

--- middleware.ts
+++ middleware.ts
@@ -7,13 +7,17 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect()
+    const { userId } = await auth()
+    if (!userId) {
+      const { redirectToSignIn } = await auth()
+      return redirectToSignIn()
+    }
   }
 })
 
 export const config = {
-    matcher: [
-      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      '/(api|trpc)(.*)',
-    ],
-  }
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
