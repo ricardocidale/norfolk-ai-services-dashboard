520c53b415544bf153ccf89f815d80d075bf9e0a
Ricardo Cidale <ricardo.cidale@norfolk.ai>
2026-03-29 03:22:41 -0500

    fix: correct middleware matcher regex

--- middleware.ts
+++ middleware.ts
@@ -12,8 +12,8 @@ export default clerkMiddleware(async (auth, request) => {
 })
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
-}
+    matcher: [
+      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+      '/(api|trpc)(.*)',
+    ],
+  }
